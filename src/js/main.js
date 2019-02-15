4c3207496abb002b19be0e9a0dea4c0827fb5ddd
pushenko-alex <fun.work.js@gmail.com>
2019-02-15 15:37:26 +0300

    up

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 (function($) {
- 
+
+$('.js-open').on('click', function(e){
+	e.preventDefault();
+	$('.menu').addClass('mob-menu');
+	$('.js-close').addClass('close');
+})
+$('.js-close').on('click', function () {
+	$('.menu').removeClass('mob-menu');
+	$('.close').removeClass('close');
+});
+$('.js-slider').slick({
+	infinite: true,
+	slidesToShow: 3,
+	prevArrow: '<div class="slick-prev" style="background-color: red"></div>',
+    nextArrow: '<div class="slick-next" style="background-color: black"></div>'
+});
 })(jQuery);
